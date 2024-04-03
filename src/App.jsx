a82851b858668c2d226b194037de54fd851a8dad
carlosalevela <carlosvelasco21347@gmail.com>
2024-04-03 09:18:26 -0500

    porfavorsubete

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <main className="font-roboto flex justify-center items-center w-screen h-screen">
+      <h1 className="text-3xl font-bold text-red-700">hello world</h1>
+    </main>
+  )
+}
+
+export default App
