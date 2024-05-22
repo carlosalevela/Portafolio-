08cf485414a055b13af2557ed05836370e7917b8
Carlos Velasco <carlosvelasco21347@gmail.com>
2024-05-21 22:30:34 -0500

    subiendo colores y fondos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,39 @@
+
 const Header = () => {
-    return (
-      <header className='fixed xl:block w-full py-4 lg:px-0 px-5 z-[999] duration-300'>
-        <nav className='flex justify-between items-center max-w-6xl mx-auto px-2'>
-          <div className='flex gap-4 items-center'>
-            <div className='bg-primary text-white rounded-full size-10 text-xl grid place-items-center'>
-            </div>
-            <div>
-              <h4 className='font-bold text-lg uppercase'>Carlos Velasco</h4>
-              <p className='text-xs'>Profile</p>
-            </div>
+  return (
+    <header className='fixed w-full py-4 z-[999] duration-300 bg-blue-600'>
+      <nav className='flex justify-between items-center max-w-6xl mx-auto px-2'>
+        <div className='flex gap-4 items-center'>
+          <div className='bg-primary text-white rounded-full size-10 text-xl grid place-items-center'>
+            <img src='Logo' alt='Logo' className='w-8 h-8' />
           </div>
-          <ul className='gap-10 md:flex hidden hover:*:text-primary *:duration-200'>
-            <li>
-              <a href='#home'>Home</a>
-            </li>
-            <li>
-              <a href='#about'>About Us</a>
-            </li>
-            <li>
-              <a href='#projects'>Projects</a>
-            </li>
-            <li>
-              <a href='#contact'>Contact Us</a>
-            </li>
-            <li className='theme-switch'>
-              <i className='fa-solid fa-circle-half-stroke cursor-pointer'></i>
-            </li>
-          </ul>
-          <div className='flex items-center gap-6'>
-            <a href='#contact'>
-              <button className='btn btn-outline md:!flex !hidden'>
-                <i className='fa-regular fa-paper-plane'></i> Let's Talk
-              </button>
-            </a>
-            <span className='theme-switch md:hidden'>
-              <i className='fa-solid fa-circle-half-stroke cursor-pointer'></i>
-            </span>
-            <span id='menubar' className='cursor-pointer md:hidden text-xl'>
-              <i className='fa-solid fa-bars'></i>
-            </span>
+          <div>
+            <h4 className='font-bold text-lg uppercase text-white'>Carlos Velasco</h4>
+            <p className='text-xs text-white'>Profile</p>
           </div>
-        </nav>
-      </header>
-    );
-  };
-  
-  export default Header;
-  
+        </div>
+        <ul className='gap-10 md:flex hidden'>
+          <li>
+            <a href='#projects' className='text-white hover:text-gray-300'>Projects</a>
+          </li>
+          <li>
+            <a href='#about' className='text-white hover:text-gray-300'>Sobre Mi</a>
+          </li>
+          <li>
+            <a href='#skills' className='text-white hover:text-gray-300'>Habilidades</a>
+          </li>
+          <li>
+            <a href='#thanks' className='text-white hover:text-gray-300'>Agradecimiento</a>
+          </li>
+        </ul>
+        <div className='flex items-center gap-6'>
+          <span id='menubar' className='cursor-pointer md:hidden text-xl text-white'>
+            <i className='fa-solid fa-bars'></i>
+          </span>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
