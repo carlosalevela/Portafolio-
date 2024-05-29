31b70f4bae3c2f4f595d29a48df6d61499f151ec
Carlos Velasco <carlosvelasco21347@gmail.com>
2024-05-29 00:15:00 -0500

    subo final de diseño

--- src/app/sections/Skills.jsx
+++ src/app/sections/Skills.jsx
@@ -7,49 +7,48 @@ import vercel from '../../assets/images/brand-vercel.png';
 import vite from '../../assets/images/brand-vite.png';
 
 const Skills = () => {
-    return (
-      <section id='skills' className='container mx-auto min-h-screen flex flex-col items-center justify-center px-5 text-white'>
-        <div className='pb-10'>
-          <div className='text-center'>
-            <h3 className='text-3xl font-bold'>HABILIDADES</h3>
-            <p className='px-2 mt-3 text-gray-500'>
-              En el recorrido de la carrera he aprendido bastante aquí están algunas de las habilidades que he desarrollado y pulido a lo largo de mi carrera.
-            </p>
+  return (
+    <section id='skills' className='container mx-auto min-h-screen flex flex-col items-center justify-center px-5 text-white'>
+      <div className='pb-10'>
+        <div className='text-center'>
+          <h3 className='text-3xl font-bold'>HABILIDADES</h3>
+          <p className='px-2 mt-3 text-gray-500'>
+            En el recorrido de la carrera he aprendido bastante aquí están algunas de las habilidades que he desarrollado y pulido a lo largo de mi carrera.
+          </p>
+        </div>
+        <div className='mt-12 grid grid-cols-2 md:grid-cols-4 gap-6 max-w-4xl mx-auto'>
+          <div className='flex flex-col items-center'>
+            <img src={java} alt='JavaScript Logo' className='h-20 w-20 mb-2' />
+            <p className='text-sm text-black'>JavaScript</p>
           </div>
-          <div className='mt-12 grid grid-cols-2 md:grid-cols-4 gap-6'>
-            <div className='flex flex-col items-center'>
-              <img src={java} alt='JavaScript Logo' className='h-20 w-20 mb-2' />
-              <p className='text-sm text-black'>JavaScript</p>
-            </div>
-            <div className='flex flex-col items-center'>
-              <img src={angu} alt='Angular Logo' className='h-20 w-20 mb-2' />
-              <p className='text-sm text-black'>Angular</p>
-            </div>
-            <div className='flex flex-col items-center'>
-              <img src={django} alt='Django Logo' className='h-20 w-20 mb-2' />
-              <p className='text-sm text-black'>Django</p>
-            </div>
-            <div className='flex flex-col items-center'>
-              <img src={react} alt='React Logo' className='h-20 w-20 mb-2' />
-              <p className='text-sm text-black'>React</p>
-            </div>
-            <div className='flex flex-col items-center'>
-              <img src={vercel} alt='Vercel Logo' className='h-20 w-20 mb-2' />
-              <p className='text-sm text-black'>Vercel</p>
-            </div>
-            <div className='flex flex-col items-center'>
-              <img src={vite} alt='Vite Logo' className='h-20 w-20 mb-2' />
-              <p className='text-sm text-black'>Vite</p>
-            </div>
-            <div className='flex flex-col items-center'>
-              <img src={git} alt='Git Logo' className='h-20 w-20 mb-2' />
-              <p className='text-sm text-black'>GitHub</p>
-            </div>
+          <div className='flex flex-col items-center'>
+            <img src={angu} alt='Angular Logo' className='h-20 w-20 mb-2' />
+            <p className='text-sm text-black'>Angular</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <img src={django} alt='Django Logo' className='h-20 w-20 mb-2' />
+            <p className='text-sm text-black'>Django</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <img src={react} alt='React Logo' className='h-20 w-20 mb-2' />
+            <p className='text-sm text-black'>React</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <img src={vercel} alt='Vercel Logo' className='h-20 w-20 mb-2' />
+            <p className='text-sm text-black'>Vercel</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <img src={vite} alt='Vite Logo' className='h-20 w-20 mb-2' />
+            <p className='text-sm text-black'>Vite</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <img src={git} alt='Git Logo' className='h-20 w-20 mb-2' />
+            <p className='text-sm text-black'>GitHub</p>
           </div>
         </div>
-      </section>
-    );
-  };
-  
-  export default Skills;
-  
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
